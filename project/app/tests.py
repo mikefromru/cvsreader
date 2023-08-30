d881060e9d6ae92a3f46113fc258d372841bd237
mikefromru <googlockgood@gmail.com>
2023-08-30 03:21:28 +0300

    Added test on post and get requests

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from . models import Client
+from django.urls import reverse
 
-# Create your tests here.
+class TestClientApiView(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('cvs-file')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertAlmostEquals(200, response.status_code)
+
+    def test_post(self):
+        files = {'csv_file': open('deals.csv', 'rb')}
+        response = self.client.post(self.url, data=files)
+        self.assertAlmostEquals(201, response.status_code)
